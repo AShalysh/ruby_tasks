be08098c0fbc9a66100d653c329ef31f4aee6256
Anastasia <anastasia.shalysh@gmail.com>
2018-03-29 01:02:35 -0700

    lesson1 with final changes

--- lesson1/rectang_triangle.rb
+++ lesson1/rectang_triangle.rb
@@ -1,4 +1,4 @@
-puts "Lets check if it is a rectangular triangle: "
+puts "Lets check the type of our triangle: "
 
 puts "Please enter the length of the A side: "
 a_side = gets.chomp.to_f
@@ -9,29 +9,28 @@ b_side = gets.chomp.to_f
 puts "Please enter the length of the C side: "
 c_side = gets.chomp.to_f
 
-if a_side == b_side && a_side == c_side
-  puts "it is an equilateral and isosceles triangle."
-elsif a_side > b_side && a_side > c_side
+if a_side > b_side && a_side > c_side
   hypotenuse = a_side
   cathetus_one = b_side
   cathetus_two = c_side
 elsif b_side > a_side && b_side > c_side
   hypotenuse = b_side
   cathetus_one = a_side
-  cathetus_two = c_side 
-elsif c_side > a_side && c_side > b_side
+  cathetus_two = c_side
+else
   hypotenuse = c_side
   cathetus_one = b_side
-  cathetus_two = a_side 
-else 
-  puts "It is not a rectangular triangle."
+  cathetus_two = a_side
 end
 
-if hypotenuse && hypotenuse**2 == cathetus_one**2 + cathetus_two**2
-  puts "It is a rectangular triangle."
-  is_rect = true
-end
+is_rect = hypotenuse**2 == cathetus_one**2 + cathetus_two**2
 
 if is_rect && cathetus_one == cathetus_two
-  puts "it is an isosceles triangle."
+  puts "It is a rectangular and isosceles triangle."
+elsif is_rect
+  puts "It is a rectangular triangle."
+elsif hypotenuse == cathetus_one && hypotenuse == cathetus_two
+  puts "it is an equilateral triangle."
+else
+  puts "It is not a rectangular triangle."
 end

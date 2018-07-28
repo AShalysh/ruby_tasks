a67dfb92f2cc9b9d444a8b5013def92ae1bc1d80
Anastasia <anastasia.shalysh@gmail.com>
2018-07-27 22:54:12 -0700

    validation changes

--- lesson8/validation.rb
+++ lesson8/validation.rb
@@ -11,35 +11,17 @@ module Validation
       @validations ||= []
     end
 
-    def validations=(value)
-      @validations = value
-    end
-
     def validate(*params)
-      self.validations = self.validations << params
+      self.validations << {name: params[0], type: params[1], extra: params[2]}
     end
   end
 
   module InstanceMethods
     def validate!
       self.class.validations.each do |validation|
-        attr_name = validation[0]
-        validation_type = validation[1]
-
-        case validation_type.to_s
-        when "presence"
-          raise "#{attr_name} can't be nil" if self.send(attr_name).nil?
-        when "type"
-          class_type = validation[2]
-          raise "#{attr_name} is not type #{class_type}" if !self.send(attr_name).is_a?(class_type)
-        when "format"
-          regex = validation[2]
-          raise "#{attr_name} does not have the correct format" if self.send(attr_name) !~ regex
-        else
-          raise "Validation type is incorrect"
-        end
+        attr_name = validation[:name]
+        self.send("#{validation[:type]}_validate!", { name: attr_name, value: self.send(attr_name), extra: validation[:extra] })
       end
-      true
     end
 
     def valid?
@@ -49,6 +31,17 @@ module Validation
       false
     end
 
+    def presence_validate!(params)
+      raise "#{params[:name]} can't be nil" if params[:value].nil?
+    end
+
+    def type_validate!(params)
+      raise "#{params[:name]} is not type #{params[:extra]}" if !params[:value].is_a?(params[:extra])
+    end
+
+    def format_validate!(params)
+      raise "#{params[:name]} does not have the correct format" if params[:value] !~ params[:extra]
+    end
   end
  # class A; include Validation; attr_accessor :name; validate :name, :presence; validate :name, :type, String; validate :name, :format, /^[a-z0-9]{3}-?[a-z0-9]{2}$/i; end;
 end
